400515cd73d469c8bc252a4c37d4cfa890ca8e42
Eric Groom <bcgroom@gmail.com>
2017-11-13 21:17:23 -0800

    basic functionality, handles light, kda, and time played

--- bot.py
+++ bot.py
@@ -4,6 +4,13 @@ import asyncio
 import logging
 
 import keys
+import destiny
+
+from dev_utils import json_browser
+
+session = aiohttp.ClientSession()
+
+api = destiny.API(keys.BUNGIE_KEY, session)
 
 logging.basicConfig(level=logging.INFO)
 client = discord.Client()
@@ -29,5 +36,26 @@ async def on_message(message):
     elif message.content.startswith('!sleep'):
         await asyncio.sleep(5)
         await client.send_message(message.channel, 'Done sleeping')
+    elif message.content.startswith('!time'):
+        tokens = message.content.split()
+        account = tokens[1]
+        resp = await client.send_message(message.channel, f'Getting time played for {account}...')
+        user = await api.get_user_from_battlenet(account)
+        json_browser(user.json)
+        await client.edit_message(resp, f'{account} has played for {user.time_played}')
+    elif message.content.startswith('!light'):
+        tokens = message.content.split()
+        account = tokens[1]
+        resp = await client.send_message(message.channel, f'Getting highest light for {account}...')
+        user = await api.get_user_from_battlenet(account)
+        await client.edit_message(resp, f'{account}\'s highest light character has {user.highest_light} light')
+    elif message.content.startswith('!kda'):
+        tokens = message.content.split()
+        account = tokens[1]
+        resp = await client.send_message(message.channel, f'Getting {account}\'s kda...')
+        user = await api.get_user_from_battlenet(account)
+        await client.edit_message(resp, f'{account}\'s PvP kda is {user.kda}')
+
 
 client.run(keys.BOT_TOKEN)
+
---
+++ destiny.py
@@ -0,0 +1,33 @@
+import urllib
+
+class API:
+    '''
+    class for interacting with the api
+    '''
+    def __init__(self, token, session):
+        self.token = token
+        self.base_url = 'https://www.bungie.net/Platform/Destiny2/'
+        self.headers = {'X-API-Key': token}
+        self.session = session
+
+    async def get_bungie_id_from_battlenet(self, battlenet):
+        battlenet_encoded = urllib.parse.quote(battlenet)
+        url = f'{self.base_url}SearchDestinyPlayer/4/{battlenet_encoded}'
+        resp = await self.session.get(url, headers=self.headers)
+        json = await resp.json()
+        return json['Response'][0]['membershipId']
+
+    async def get_user_from_battlenet(self, battlenet):
+        battlenet_encoded = urllib.parse.quote(battlenet)
+        membership_id = await self.get_bungie_id_from_battlenet(battlenet)
+        url = f'{self.base_url}4/Account/{membership_id}/Stats/'
+        resp = await self.session.get(url, headers=self.headers)
+        json = await resp.json()
+        return User(json['Response'])
+
+class User:
+    def __init__(self, json):
+        self.json = json
+        self.time_played = json['mergedAllCharacters']['merged']['allTime']['secondsPlayed']['basic']['displayValue']
+        self.highest_light = json['mergedAllCharacters']['merged']['allTime']['highestLightLevel']['basic']['displayValue']
+        self.kda = json['mergedAllCharacters']['results']['allPvP']['allTime']['efficiency']['basic']['displayValue']
